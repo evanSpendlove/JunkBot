209bf5fc3d1f9c7f3ba9d43f8792184dd1444312
reubsmull <reubsmull@gmail.com>
2020-02-21 14:03:32 +0000

    [ADDED] BoardTest file with initial comment header

---
+++ JunkBot/src/test/java/BoardTest.java
@@ -0,0 +1,28 @@
+
+import org.junit.jupiter.api.Test;
+import scrabbleGame.Frame;
+import scrabbleGame.Pool;
+import scrabbleGame.Tile;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    BoardTest JUnit Test Class
+
+    Purpose: Unit-Testing the Board Class of the scrabbleGame package.
+    Summary: This class attempts to thoroughly test the Board Class and its methods to ensure they
+             are robust.
+
+    Team: JunkBot
+    Members: Reuben Mulligan (18733589), Evan Spendlove (18492656), Cal Nolan(18355103)
+
+    Author: Reuben Mulligan, Cal Nolan
+    Version: 1.0.0
+    Since: 21-02-2020
+ */
+
+public class BoardTest
+{
+
+}
