cd84c9cea569bf5cdbbeed2e846348870bf150e3
reubsmull <reubsmull@gmail.com>
2020-03-02 16:06:28 +0000

    [Updated] Started initial commenting for Scrabble Class

--- JunkBot/src/main/java/scrabbleGame/Scrabble.java
+++ JunkBot/src/main/java/scrabbleGame/Scrabble.java
@@ -1,5 +1,16 @@
 package scrabbleGame;
 
+/**
+ * <h1>Scrabble Class</h1>
+ * This represents the game itself, runs initial setup
+ * <p></p>
+ * Team: JunkBot
+ *  * Members: Reuben Mulligan (18733589), Evan Spendlove (18492656), Cal Nolan(18355103)
+ * @author Cal Nolan
+ * @version 1.0.0
+ * @since 02-03-2020
+ */
+
 public class Scrabble {
 
 }
